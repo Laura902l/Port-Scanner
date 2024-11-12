9d0a09f26aac8e4e673b399aeb9ea7ee0ad369c8
Laura <121919112+Laura902l@users.noreply.github.com>
2024-11-12 13:31:51 +0500

    Create common_ports.py

---
+++ common_ports.py
@@ -0,0 +1,17 @@
+# Dictionary of common ports and their associated services
+PORTS = {
+    20: "ftp-data",
+    21: "ftp",
+    22: "ssh",
+    23: "telnet",
+    25: "smtp",
+    53: "dns",
+    67: "dhcp",
+    80: "http",
+    110: "pop3",
+    143: "imap",
+    443: "https",
+    3389: "rdp",
+    8080: "http-proxy",
+    # Add more common ports here as necessary
+}
